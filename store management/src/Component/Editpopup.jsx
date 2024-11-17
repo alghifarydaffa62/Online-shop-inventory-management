8bb90aa927ad83bb8050b86432c891f6d14ca3fc
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-11-17 19:50:35 +0700

    edit feature still broken

---
+++ store management/src/Component/Editpopup.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import "../style/style.css";
+
+function EditPopup({ product, onClose, onSave }) {
+    const [formData, setFormData] = useState({
+        name: "",
+        image: "",
+        sku: "",
+        price: 0,
+        quantity: 0,
+        marketplace: ""
+    });
+
+    useEffect(() => {
+        if (product) {
+            setFormData({
+                name: product.name,
+                image: product.image,
+                sku: product.sku,
+                price: product.price,
+                quantity: product.quantity,
+                marketplace: product.marketplace
+            });
+        }
+    }, [product]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSave({ ...formData, id: product.id });
+        onClose(e)
+    };
+
+    if (!product) return null;
+
+    return (
+        <div className="overlay">
+            <form className="edit-popup" onSubmit={handleSubmit}>
+                <h2>Edit Produk</h2>
+                <div className="popup-input">
+                    <div>
+                        <label>Nama produk:</label><br/>
+                        <input type="text" name="name" value={formData.name} onChange={handleChange} required /><br/>
+                        
+                        <label>Gambar produk:</label><br/>
+                        <input type="text" name="image" value={formData.image} onChange={handleChange} required /><br/>
+                        
+                        <label>Sku produk:</label><br/>
+                        <input type="text" name="sku" value={formData.sku} onChange={handleChange} required /><br/>
+                    </div>
+                    
+                    <div>
+                        <label>Harga produk:</label><br/>
+                        <input type="number" name="price" value={formData.price} onChange={handleChange} required /><br/>
+                        
+                        <label>Kuantitas produk:</label><br/>
+                        <input type="number" name="quantity" value={formData.quantity} onChange={handleChange} required /><br/>
+                        
+                        <label>Marketplace produk:</label><br/>
+                        <input type="text" name="marketplace" value={formData.marketplace} onChange={handleChange} required /><br/>
+                    </div>
+                    
+                </div>
+                <button type="submit">Simpan</button>
+                <button onClick={onClose}>X</button>
+            </form>
+        </div>
+    );
+}
+
+export default EditPopup
--- store management/src/Component/Homepage.jsx
+++ store management/src/Component/Homepage.jsx
@@ -4,20 +4,24 @@ import NewProduct from './Newproduct'
 import ProductList from './productList'
 import Navbar from './Navbar'
 import Viewpopup from './Viewpopup'
+import EditPopup from './Editpopup'
 import '../style/style.css'
 
 class HomePage extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             product: product(),
             viewProduct: null,
-        }
+            editProduct: null, // New state for edit product
+        };
 
-        this.onDeletehandler = this.onDeletehandler.bind(this)
-        this.AddProducthandler = this.AddProducthandler.bind(this)
-        this.ViewHandler = this.ViewHandler.bind(this)
+        this.onDeletehandler = this.onDeletehandler.bind(this);
+        this.AddProducthandler = this.AddProducthandler.bind(this);
+        this.ViewHandler = this.ViewHandler.bind(this);
+        this.EditHandler = this.EditHandler.bind(this); // New handler
+        this.saveEditHandler = this.saveEditHandler.bind(this); // New save handler
     }
 
     onDeletehandler(id) {
@@ -69,59 +73,22 @@ class HomePage extends React.Component {
         })
     }
 
-    // editHandler(id) {
-    //     const editproduct = this.state.product.find(item => item.id == id)
+    EditHandler(id) {
+        const selectedProduct = this.state.product.find(item => item.id === id);
+        if (selectedProduct) {
+            this.setState({ editProduct: selectedProduct }, () => {
+                document.body.classList.add("no-scroll");
+            });
+        }
+    }
 
-    //     if(editproduct) {
-    //         this.setState({editProduct: editproduct}, () => {
-    //             document.body.classList.add("no-scroll")
-    //         })
-    //     }
-    // }
-    
-    // close = (event) => {
-    //     event.preventDefault()
-    //     this.setState({editProduct: null}, () => {
-    //         document.body.classList.remove("no-scroll")
-    //     })
-    // }
-
-    // EditPopUp() {
-    //     const {editProduct} = this.state
-    //     if (!editProduct) return null
-    
-    //     return (
-    //         <div className='overlay'>
-    //             <form className="view-popup"> 
-    //                 <h2>Tentang Produk</h2>
-    //                 <div className="popup-input">
-    //                     <div>
-    //                         <label for="name">Nama produk:</label><br/>
-    //                         <input type="text" value={editProduct.name}/><br/>
-
-    //                         <label for="img">Gambar produk:</label><br/>
-    //                         <img src={editProduct.image} alt="image"/><br/>
-
-    //                         <label for="sku">Sku produk:</label><br/>
-    //                         <input type="text" value={editProduct.sku} /><br/>
-    //                     </div>
-                        
-    //                     <div>
-    //                         <label for="harga">Harga produk:</label><br/>
-    //                         <input type="number" value={editProduct.price}/><br/>
-
-    //                         <label for="quantity">Kuantitas produk:</label><br/>
-    //                         <input type="number" value={editProduct.quantity}/><br/>
-
-    //                         <label for="marketplace">Marketplace produk:</label><br/>
-    //                         <input type="text" value={editProduct.marketplace}y/><br/> 
-    //                     </div>
-    //                 </div>
-    //                 <button onClick={this.close}>X</button>  
-    //             </form>
-    //         </div>  
-    //     )
-    // }
+    saveEditHandler(updatedProduct) {
+        const updatedProducts = this.state.product.map(prod =>
+            prod.id === updatedProducts.id ? updatedProducts : prod
+        );
+        localStorage.setItem('products', JSON.stringify)
+    }
+   
 
     render() {
         return(
@@ -129,7 +96,8 @@ class HomePage extends React.Component {
                 <Navbar/>
                 <NewProduct Addproduct = {this.AddProducthandler}/>
                 <Viewpopup product={this.state.viewProduct} onClose={this.closePopUp} />
-                <ProductList product={this.state.product} onDelete={this.onDeletehandler} onView={this.ViewHandler} />
+                <EditPopup product={this.state.editProduct} onSave={this.saveEditHandler} onClose={this.closePopUp}/>
+                <ProductList product={this.state.product} onDelete={this.onDeletehandler} onView={this.ViewHandler} onEdit={this.EditHandler}/>
             </div>
         )
     }
